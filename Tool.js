8fdad701442f123e4254b0104496641cb33e8ee3
Jose27699 <122585985+Jose27699@users.noreply.github.com>
2023-03-09 08:51:47 -0600

    Add files via upload

--- Tool.js
+++ Tool.js
@@ -1,66 +1,25 @@
-class Tool {
-    /**
-     * 
-     * @param {string} cadena 
-     * @returns {string}
-     */
-    static strToSlug (cadena){
-        return encodeURIComponent(cadena).replace(/%20/g, '-');
-    }
+function validarMaterias(event) {
+    event.preventDefault(); // evitar que se envíe el formulario
 
-    static numberToLetter(number){
-        let letter;
-        switch (number){
-            case 1:
-                letter = "Uno";
-                break;
-            case 2:
-                letter =  "dos";
-                break;
-            default:
-                letter = "No encontrado";
-                break;
+    let semestreSeleccionado = document.querySelector('option[name="semestre"]:checked').value;
+    let materiasSeleccionadas = document.querySelectorAll('input[name="materia"]:checked');    
+    // validar si las materias seleccionadas corresponden al semestre seleccionado
+
+    for (let i = 0; i < materiasSeleccionadas.length; i++) { 
+        let semestresPermitidos = materiasSeleccionadas[i].dataset.semestre.split(' ');
+        alert(`Agregates esta materia ${materiasSeleccionadas[i].value}`);
+        if (!semestresPermitidos.includes(semestreSeleccionado)) {
+            alert(`La materia ${materiasSeleccionadas[i].value} no está disponible para el semestre seleccionado`);
+            materiasSeleccionadas[i].checked = false;
+
         }
-        return letter;
     }
-    /**
-     * 
-     * @param {String} parameter 
-     * @param {Array} data 
-     */
-    static setDataToLocalStorage(parameter, data){
-        localStorage.setItem(parameter, JSON.stringify(data));
-    }
-    /**
-     * 
-     * @param {string} parameter 
-     * @returns {Array}
-     */
-    static getDatFromLocalStorage(parameter){
-        return JSON.parse( localStorage.getItem(parameter));
-    }
-
-    static identifyUser(user){
-        personas = getDatFromLocalStorage(personas)
-        user_ = personas.prototype.find(user);
-        return user
-    }
-
-    static getCarreraFromLocalStorage(parameter="carreras"){
-        let carreras = []; 
-        let carrerasArray = JSON.parse( localStorage.getItem(parameter));
-        if( carrerasArray != null){ 
-         carrerasArray.forEach(element => {
-            let object =  new Carrera();
-            object.Nombre = element.Nombre;
-            object.Generacion = element.Generacion;
-            carreras.push(object);
-        });
-        
-        }
-        return carreras;
-    }
-
-    
-
 }
+
+
+function mostrarMaterias() {
+    let Eleccion1 = document.gerySelector('input[name="materia"]:checked').value;
+    let Eleccion2 = document.getElementById('input[name="materia"]:checked').value;
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = "Materias seleccionadas: " + Eleccion1  + " , "+ Eleccion2;
+  }
